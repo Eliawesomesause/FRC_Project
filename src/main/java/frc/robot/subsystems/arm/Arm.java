fa726943d34e971924d17db992adb4a0b1ff1149
Eliawesomesause <eliisaking71@gmail.com>
2025-11-24 08:52:23 +0200

    more PID

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.arm;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -8,11 +7,15 @@ public class Arm extends SubsystemBase {
     public Arm() {
     }
 
-    public static Angle getPosition() {
+    public Angle getPosition() {
         return ArmConstants.CANCODER.getAbsolutePosition().getValue();
     }
 
-    public static PIDController pidController(double kp, double ki, double kd) {
-        return new PIDController(kp, ki, kd);
+    public double getAngleAsDouble() {
+        return ArmConstants.ANGLE_STATUS_SIGNAL.refresh().getValueAsDouble();
+    }
+
+    public void doPIDCalculations() {
+        ArmConstants.MOTOR.set(ArmConstants.PID_CONTROLLER.calculate(getAngleAsDouble(), 67));
     }
 }
--- src/main/java/frc/robot/subsystems/arm/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/arm/ArmConstants.java
@@ -1,17 +1,21 @@
 package frc.robot.subsystems.arm;
 
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.units.measure.Angle;
 
 public class ArmConstants {
     private static final int MOTOR_ID = 1;
     private static final int CANCODER_ID = 1;
     static final TalonFX MOTOR = new TalonFX(MOTOR_ID);
     static final CANcoder CANCODER = new CANcoder(CANCODER_ID);
+    static final PIDController PID_CONTROLLER = new PIDController(1, 0, 0);
+    static final StatusSignal<Angle> ANGLE_STATUS_SIGNAL = CANCODER.getPosition();
 
     static {
         final TalonFXConfiguration config = new TalonFXConfiguration();
